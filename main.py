65f5c672203238f978a9307aae26ebf5c45ba458
Ahmed Gamal <ahmed.gamal@dc.mobilityp.com>
2026-04-22 14:57:32 +0200

    update

--- main.py
+++ main.py
@@ -2,34 +2,46 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 from concurrent.futures import ThreadPoolExecutor 
-
-
+import polars as pl
 # %%
-@st.cache_data
+print(st.secrets["connection_string"])
+
 def load_data():
-    return pd.read_excel('mobility_ksa_fact_stock_ledger_entry.xlsx')
-df=load_data()
+    return pl.read_database_uri(
+    query="SELECT source,item_code,qty FROM ksa.fact_sales_invoice",
+    uri=st.secrets['connection_string'],
+    schema_overrides={
+        "source": pl.Utf8,
+        "item_code": pl.Utf8,
+    }
+)
+
+df = load_data()
 
 st.markdown("<h1 style='color:#042771;'>Mobility Pro</h1>", unsafe_allow_html=True)
 st.markdown("<h2 style='color:#042771;'>Download Reports :)</h2>", unsafe_allow_html=True)
 col1, col2 = st.columns(2)
-options = sorted(df["voucher_type"].dropna().unique())
-customers= sorted(df["item_code"].unique())
-
+options = sorted(df["source"].unique().to_list())
+customers = (df.select("item_code")
+      .unique()
+      .sort("item_code")
+      .to_series()
+      .to_list()
+)
 with col1:
     group = st.selectbox(
-    "voucher_type",
+    "item_code",
     options=options,
     index=None,
-    placeholder="Select voucher_type"
+    placeholder="Select item_code"
 )
 
 with col2:
     customer = st.selectbox(
-    "item_code",
+    "source",
     options=customers,
     index=None,
-    placeholder="Select item_code"
+    placeholder="Select source"
 )
 
 # Sample DataFra
@@ -37,7 +49,7 @@ with col2:
 def to_excel(df):
     output = BytesIO()
     with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
-        df.to_excel(writer, index=False, sheet_name='Sheet1')
+        df.to_pandas().to_excel(writer, index=False, sheet_name='Sheet1')
     processed_data = output.getvalue()
     return processed_data
 
